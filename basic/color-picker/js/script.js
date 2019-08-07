886fdc88a57076f2ea9d2749abaa5f59bc202f05
nqvuong1998 <nqvuong@gmail.com>
2019-08-08 01:40:33 +0700

    basic

--- basic/color-picker/js/script.js
+++ basic/color-picker/js/script.js
@@ -1,5 +1,29 @@
 
+let color = {
+    "red": 128,
+    "green": 128,
+    "blue": 128
+}
 
+function changeColor(event){
+    let code = event.value;
+    let id = event.id;
+
+    let hex = fullColorToHex(color['red'], color['green'], color['blue']);
+    color[id]=code;
+    document.querySelector("#"+id+"-code").innerHTML=code;
+    document.querySelector(".hex").innerHTML=hex;
+    setColorHexCode();
+    document.querySelector(".result").style.backgroundColor = hex;
+}
+
+function setColorHexCode(){
+    let o = Math.round(((color['red'] * 299) +
+                      (color['green'] * 587) +
+                      (color['blue'] * 114)) / 1000);
+    let fore = (o > 125) ? 'black' : 'white';
+    document.querySelector(".hex").style.color=fore;
+}
 
 function rgbToHex(value) {
     let hex = Number(value).toString(16);
@@ -15,3 +39,51 @@ function fullColorToHex(r, g, b) {
     let blue = rgbToHex(b);
     return "#" + red + green + blue;
 }
+
+function getWidth() {
+    return Math.max(
+      document.body.scrollWidth,
+      document.documentElement.scrollWidth,
+      document.body.offsetWidth,
+      document.documentElement.offsetWidth,
+      document.documentElement.clientWidth
+    );
+}
+
+window.addEventListener('resize', function(event){
+    let width = getWidth();
+    console.log(width);
+    if(width<=400){
+        changeWidthContent(100);
+        changeMarginTopContent(20);
+    }
+    else if(width<=800){
+        changeWidthContent(85);
+        changeMarginTopContent(13);
+    }
+    else if(width<=1000){
+        changeWidthContent(65);
+        changeMarginTopContent(14);
+    }
+    else if(width<=1200){
+        changeWidthContent(45);
+        changeMarginTopContent(12);
+    }
+    else{
+        changeWidthContent(35);
+        changeMarginTopContent(8);
+    }
+    
+});
+
+function changeWidthContent(percent){
+    let style = document.querySelector(".all").style;
+    style.width = percent+"%";
+}
+
+function changeMarginTopContent(percent){
+    let style = document.querySelector(".all").style;
+    style.marginTop = percent+"%";
+}
+
+
